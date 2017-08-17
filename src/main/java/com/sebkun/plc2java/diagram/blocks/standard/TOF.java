528ab2424a301cce75987eb8f2daa1a74d113657
Sebastian Kunze <kunze.seb@gmail.com>
2017-08-17 14:16:40 +0200

    added standard function blocks

---
+++ src/main/java/com/sebkun/plc2java/diagram/blocks/standard/TOF.java
@@ -0,0 +1,20 @@
+package com.sebkun.plc2java.diagram.blocks.standard;
+
+import com.sebkun.plc2java.diagram.blocks.Block;
+
+import java.util.Map;
+
+/**
+ * @author sebkun
+ */
+public class TOF extends Block {
+
+    public TOF(int executionOrderId) {
+        super(executionOrderId);
+    }
+
+    @Override
+    public Map<String, Integer> execute() {
+        return null;
+    }
+}
---
+++ src/main/java/com/sebkun/plc2java/diagram/blocks/standard/TON.java
@@ -0,0 +1,20 @@
+package com.sebkun.plc2java.diagram.blocks.standard;
+
+import com.sebkun.plc2java.diagram.blocks.Block;
+
+import java.util.Map;
+
+/**
+ * @author sebkun
+ */
+public class TON extends Block {
+
+    public TON(int executionOrderId) {
+        super(executionOrderId);
+    }
+
+    @Override
+    public Map<String, Integer> execute() {
+        return null;
+    }
+}
