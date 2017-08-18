73a53b2b42d9239c49d72a8561a98ea1d9e995e7
Sebastian Kunze <sebastian.kunze@hh.se>
2017-08-18 17:34:29 +0200

    bugfix using BOOL as input

--- src/main/java/com/sebkun/plc2java/diagram/blocks/comparison/EQ.java
+++ src/main/java/com/sebkun/plc2java/diagram/blocks/comparison/EQ.java
@@ -21,12 +21,14 @@ public class EQ extends FunctionBlock {
 
     // --- PATTERNS ---
 
-    private static final String INPUT_IN_PATTERN = "IN";
+    private static final String INPUT_IN_PATTERN = "IN"; // TODO: Implement pattern.
 
-    public EQ(int executionOrderId, List<Connector> inputList) {
+    public EQ(int executionOrderId, List<Connector> inputList, Connector out) { // TODO: add constructor without executionID
         super(executionOrderId);
 
         this.setInputList(INPUT_IN_PATTERN, inputList);
+
+        this.setOutput(OUTPUT_OUT, out);
     }
 
     @Override
@@ -38,15 +40,29 @@ public class EQ extends FunctionBlock {
             updateOutput(EQ.OUTPUT_OUT, new BOOL(true));
         } else {
 
-            Connector con = getInputs().get("IN1");
+            Connector<Boolean> out = new BOOL(true);
+
+            Connector tmp = getInputs().get("IN1");
 
             for (int i = 1; i < getInputs().size(); i++) {
 
-                Connector in = getInputs().get(EQ.INPUT_IN_PATTERN + String.valueOf(i));
+                Connector in = getInputs().get(EQ.INPUT_IN_PATTERN + String.valueOf(i + 1));
 
-                con = con.eq(in);
+                // check if pairs are non-equivalent.
+                if (!tmp.eq(in).getValue()) {
+
+                    // update output.
+                    out = new BOOL(false);
+
+                    // stop iterating input pairs.
+                    break;
+                }
+
+                // continue with next pair of inputs.
+                tmp = in;
             }
-            updateOutput(EQ.OUTPUT_OUT, con);
+
+            updateOutput(EQ.OUTPUT_OUT, out);
         }
         return outputs;
     }
