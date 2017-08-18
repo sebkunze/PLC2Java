c2d3b1d0fbebe178b3f0c8cbd55d65df5849e5b7
Sebastian Kunze <sebastian.kunze@hh.se>
2017-08-18 17:36:13 +0200

    moved eq and ne out of abstract class

--- src/main/java/com/sebkun/plc2java/diagram/connector/Connector.java
+++ src/main/java/com/sebkun/plc2java/diagram/connector/Connector.java
@@ -2,8 +2,6 @@ package com.sebkun.plc2java.diagram.connector;
 
 import com.sebkun.plc2java.diagram.connector.operators.COMPARISON;
 import com.sebkun.plc2java.diagram.connector.operators.LOGIC;
-import com.sebkun.plc2java.diagram.connector.operators.NonSupportedOperationException;
-import com.sebkun.plc2java.diagram.connector.types.BOOL;
 
 /**
  * @author sebkun
@@ -23,20 +21,6 @@ public abstract class Connector<T> implements COMPARISON, LOGIC {
     @Override
     public String toString() {
         return value.toString();
-    }
-
-    @Override
-    public Connector eq(Connector c)
-            throws NonSupportedOperationException {
-
-        return new BOOL(this.getValue() == c.getValue());
-    }
-
-    @Override
-    public Connector ne(Connector c)
-            throws NonSupportedOperationException {
-
-        return new BOOL(this.getValue() != c.getValue());
     }
 }
 
