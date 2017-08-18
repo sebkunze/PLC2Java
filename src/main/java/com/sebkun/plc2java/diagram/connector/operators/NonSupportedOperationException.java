419df56eab7d1d29028c0ad9d63717a68f716f33
Sebastian Kunze <sebastian.kunze@hh.se>
2017-08-18 15:57:39 +0200

    added exception for non-supported opertors

--- src/main/java/com/sebkun/plc2java/diagram/connector/operators/NonSupportedOperationException.java
+++ src/main/java/com/sebkun/plc2java/diagram/connector/operators/NonSupportedOperationException.java
@@ -3,5 +3,9 @@ package com.sebkun.plc2java.diagram.connector.operators;
 /**
  * Created by sebkun on 2017-08-18.
  */
-public class NonSupportedOperationException {
+public class NonSupportedOperationException extends Exception {
+
+    public NonSupportedOperationException(String mmsg) {
+
+    }
 }
